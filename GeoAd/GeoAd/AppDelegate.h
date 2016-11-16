512fca649dde80b45f2b96e18810b9438734ed39
Karthik Rao <raokarthik74@gmail.com>
2016-11-16 09:42:36 -0800

    initial commit

---
+++ GeoAd/GeoAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeoAd
+//
+//  Created by Karthik Rao on 11/16/16.
+//  Copyright © 2016 Karthik Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeoAd/GeoAd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeoAd
+//
+//  Created by Karthik Rao on 11/16/16.
+//  Copyright © 2016 Karthik Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
